da804b113ad345a371e0eafa2b46d5f72c0027c2
Koketso Gaowelwe <gwlkok001@nightmare.cs.uct.ac.za>
2026-02-26 16:13:05 +0000

    working on Check.java file

--- src/Check.java
+++ src/Check.java
@@ -1,3 +1,19 @@
+import java.io.*;
+import java.util.*;
+
 public class Check {
-    
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) { // avoid memory leaks
+            String filename = input.nextLine();
+
+            try (FileInputStream file = new FileInputStream(new File(filename))) {
+                
+            } catch (IOException e) {
+                System.out.println("Couldn't open the file");
+            }
+
+
+
+        }
+    }
 }
