67a777cd6e095a2828e492b9bec42d4c9261db82
Koketso <koketsogaowelwe2@gmail.com>
2026-04-03 15:12:34 +0200

    create a time class to evaluate time to English Language Description

--- sampleI/Converter.java
+++ sampleI/Converter.java
@@ -2,4 +2,21 @@ package sampleI;
 
 public class Converter {
 
+    public static void main(String[] args) {
+
+        try (java.util.Scanner input = new java.util.Scanner(System.in)) {
+
+            String userInput;
+            do {
+
+                System.out.println("Enter a time or quit:");
+                userInput = input.nextLine();
+
+                java.time.LocalTime time = java.time.LocalTime.parse(userInput, java.time.format.DateTimeFormatter.ofPattern("HH:mm"));
+
+                System.out.println(new TimeEvaluator(time));
+
+            } while (userInput.equalsIgnoreCase("quit"));
+        }
+    }
 }
---
+++ sampleI/TimeEvaluator.java
@@ -0,0 +1,18 @@
+package sampleI;
+
+public class TimeEvaluator {
+
+    private java.time.LocalTime time;
+
+    public TimeEvaluator(java.time.LocalTime time) {
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+
+        
+
+        return null;
+    }
+}
