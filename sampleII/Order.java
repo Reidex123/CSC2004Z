ae379ebc00fbeb257f43d67cc3536b35ab7aebe7
Koketso <koketsogaowelwe2@gmail.com>
2026-04-04 19:09:56 +0200

    started with Order.java file

--- sampleII/Order.java
+++ sampleII/Order.java
@@ -2,4 +2,13 @@ package sampleII;
 
 public class Order {
 
+    public static void main(String[] args) {
+
+        try (java.util.Scanner input = new java.util.Scanner(System.in)) {
+
+            System.out.println("Enter the name of the word lists text file:");
+            String filename = input.nextLine();
+        }
+    }
+
 }
