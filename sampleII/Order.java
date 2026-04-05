26618ba68190f08ebc03ebee2748c93bafa175bc
Koketso <koketsogaowelwe2@gmail.com>
2026-04-05 12:03:34 +0200

    implementing the logic for Order class

--- sampleII/Order.java
+++ sampleII/Order.java
@@ -8,7 +8,37 @@ public class Order {
 
             System.out.println("Enter the name of the word lists text file:");
             String filename = input.nextLine();
+
+            Read wordLists = new Read(filename);
+
+            for (String word : wordLists.list) {
+                
+            }
         }
     }
 
+}
+
+class Read {
+    public java.util.List<String> list = new java.util.ArrayList<>();
+
+    public Read(String filename) {
+        this.store(filename);
+    }
+
+    private void store(String filename) {
+
+        try (java.io.BufferedReader file = new java.io.BufferedReader(new java.io.FileReader(filename))) {
+            String line = file.readLine();
+
+            while (line != null) {
+                list.add(line);
+
+                line = file.readLine();
+            }
+        }
+        catch (java.io.IOException e) {
+            System.out.println("File not found!!");
+        }
+    }
 }
