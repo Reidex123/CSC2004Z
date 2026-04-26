4c0a753c777cca90b9999b06380168726871d4f2
Koketso <koketsogaowelwe2@gmail.com>
2026-04-26 18:58:59 +0200

    "BucketList => sorting words into their specific categories"

--- sampleIII/BucketList.java
+++ sampleIII/BucketList.java
@@ -2,4 +2,52 @@ package sampleIII;
 
 public class BucketList {
 
+    public static void main(String[] args) {
+
+        try (java.util.Scanner input = new java.util.Scanner(System.in)) {
+
+            System.out.println("Enter the name of the categories file:");
+            String filename = input.nextLine();
+
+            categories storedCategories = new categories(filename);
+
+            System.out.println("Enter a comma-separated list of words:");
+            String[] words = input.nextLine().split(", ");
+
+            for (String category : storedCategories.getCateg()) {
+
+            }
+        }
+    }
+
+}
+
+class categories {
+
+    public java.util.List<String> categ;
+
+    public categories(String filename) {
+        this.categ = new java.util.ArrayList<>();
+        this.readFile(filename);
+    }
+
+    private void readFile(String filename) {
+
+        try (java.io.BufferedReader file = new java.io.BufferedReader(new java.io.FileReader(filename))) {
+
+            String line = file.readLine();
+
+            while (line != null) {
+                this.categ.add(line);
+                line = file.readLine();
+            }
+        } catch (java.io.IOException e) {
+            System.out.println("file not found");
+        }
+    }
+
+    public java.util.List<String> getCateg() {
+
+        return this.categ;
+    }
 }
